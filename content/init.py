14a41f3a6e744ba148390119da7006c7e227d861
leonidasf300 <86639340+leonidasf300@users.noreply.github.com>
2022-12-08 08:06:06 -0500

    Create init.py
    first commit

---
+++ content/init.py
@@ -0,0 +1,33 @@
+# ------ COURSE PARAMS ------
+#course_id = '\S*deeplearning\S*'
+github_repo = 'leonidasf300/dl22_2'
+#endpoint = 'https://m5knaekxo6.execute-api.us-west-2.amazonaws.com/dev-v0001/rlxmooc'
+# ------ COURSE PARAMS ------
+
+def init(force_download=False):
+    from IPython.display import display, HTML
+    js = """
+<meta name="google-signin-client_id"
+      content="461673936472-kdjosv61up3ac1ajeuq6qqu72upilmls.apps.googleusercontent.com"/>
+<script src="https://apis.google.com/js/client:platform.js?onload=google_button_start"></script>
+    """
+
+    display(HTML(js))
+
+    if force_download or not os.path.exists("local"):
+        print("replicating local resources")
+        dirname = github_repo.split("/")[-1]+"-main/"
+        if os.path.exists(dirname):
+            shutil.rmtree(dirname)
+        r = requests.get(zip_file_url)
+        z = zipfile.ZipFile(io.BytesIO(r.content))
+        z.extractall()
+        if os.path.exists("local"):
+            shutil.rmtree("local")
+        if os.path.exists(dirname+"/content/local"):
+            shutil.move(dirname+"/content/local", "local")
+        elif os.path.exists(dirname+"/local"):
+            shutil.move(dirname+"/local", "local")
+        shutil.rmtree(dirname)
+        
+import requests, zipfile, io, os, shutil, subprocess 
